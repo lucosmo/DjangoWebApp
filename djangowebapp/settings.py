6468da2c351c558a1802e0f6a78be086b12df4c5
lucosmo <lukasz.kosmaczewski@gmail.com>
2025-05-06 15:44:51 +0100

    add styles into static files & place footer

--- djangowebapp/settings.py
+++ djangowebapp/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,6 +125,7 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_UPLOADS = MEDIA_ROOT / 'uploads'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
